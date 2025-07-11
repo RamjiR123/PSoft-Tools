10fcece019202b0dffa7d80c2deb5c9c303daba6
ramjir123 <itsramjir123@gmail.com>
2025-07-11 15:25:41 -0400

    added documentation for navbar files

--- psoft-tools/src/components/Navbar.tsx
+++ psoft-tools/src/components/Navbar.tsx
@@ -6,30 +6,42 @@ import "./Navbar.css";
 export default function Navbar() {
     const [isImageHighlighted, setIsImageHighlighted] = useState(false);
 
+    //if the user clicks on the logo
     const handleMouseDown = () => {
         setIsImageHighlighted(true);
     };
 
+    //when user let go of click on the logo
     const handleMouseUp = () => {
         setIsImageHighlighted(false);
         
         //to automatically adjust to the current semester
         const today: Date = new Date();
+
+        //full webpage should look like "cs.rpi.edu/academics/courses/semester+year/csci2600"
+        //eg. cs.rpi.edu/academics/courses/summer25/csci2600
         let semester = "https://www.cs.rpi.edu/academics/courses/"
+
+        //jan 1 - may 10
         if(today.getMonth() <= 4 && today.getDate() >= 10)
             semester += "spring";
 
+        //august 22 - december 31
         else if(today.getMonth()>=7 && today.getDate() >= 22)
             semester += "fall";
 
+        //may 11 - august 21
         else
             semester += "summer";
 
-        semester += today.getFullYear().toString().substring(2);
-        semester += "/csci2600/";
+        
+        semester += today.getFullYear().toString().substring(2); //adds last 2 digits of full year (eg. '25' for '2025')
+        semester += "/csci2600/"; //adds class code to url
         window.location.href = semester;
     };
 
+    //adds functionality to all buttons. logo first, then the others. others have two states: active and inactive ->
+    //for whether the webpage is the current one being displayed.
     return (
         <>
             <div 
@@ -48,7 +60,7 @@ export default function Navbar() {
                         onMouseUp={handleMouseUp}
                         onMouseLeave={() => setIsImageHighlighted(false)}
                     />
-
+                
                 </div>
                 <div className="options">
                     <NavLink
